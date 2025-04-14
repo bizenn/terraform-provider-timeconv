56dc37643c0880b7796f8e479e66328f7bd63baa
Tatsuya BIZENN <bizenn@gmail.com>
2025-04-14 11:00:21 +0900

    Drop go:generate that be duplicated with tools.

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) Tatsuya BIZENN <bizenn@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -11,16 +11,6 @@ import (
 	"github.com/bizenn/terraform-provider-timeconv/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
-
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
 	// these will be set by the goreleaser configuration
