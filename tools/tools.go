85496a27620daef7a34a6b25411c720fc223c03a
Tatsuya BIZENN <bizenn@gmail.com>
2025-04-14 10:06:03 +0900

    Update tools.

--- tools/tools.go
+++ tools/tools.go
@@ -1,11 +1,17 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
+// Generate copyright headers
+//go:generate go run github.com/hashicorp/copywrite headers -d .. --config ../.copywrite.hcl
 
-//go:build tools
+// Format Terraform code for use in documentation.
+// If you do not have Terraform installed, you can remove the formatting command, but it is suggested
+// to ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ../examples/
 
+// Generate documentation.
+//
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name timeconv
 package tools
 
 import (
-	// Documentation generation
+	_ "github.com/hashicorp/copywrite"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
